aa5f686731f5aa10bd32b1cf0ab141e6e62f2205
Zhen She <neal.seah@gmail.com>
2018-12-02 17:59:29 +0800

    feat: can bind onclick function from class

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  increase=() => {
+    alert("increase number by 1")
+  }
+  decrease=() => {
+    alert("decrease number by 1")
+  }
   render() {
     return (
       <div>
-        <button onClick={() => alert('+')}>+</button>
+        <button onClick={this.increase}>+</button>
         <span>0</span>
-        <button onClick={() => alert('-')}>-</button>
+        <button onClick={this.decrease}>-</button>
       </div>
     );
   }
